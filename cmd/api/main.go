009fc21b055f4af5997c2667783cfa20bb421dba
Taterbro <135230103+Taterbro@users.noreply.github.com>
2026-04-18 00:07:51 +0100

    cors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -18,9 +18,26 @@ func main() {
 
 	server := &http.Server{
 		Addr:    ":8080",
-		Handler: mux,
+		Handler: cors(mux),
 	}
 
 	log.Println("Server running on http://localhost:8080")
 	log.Fatal(server.ListenAndServe())
+}
+
+func cors(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+        // Handle preflight requests
+        if r.Method == http.MethodOptions {
+            w.WriteHeader(http.StatusNoContent)
+            return
+        }
+
+        next.ServeHTTP(w, r)
+    })
 }
